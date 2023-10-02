09476d4fa35659435362972771691573be3ffafc
Filip Bercik <xbercikf@stuba.sk>
2023-10-02 18:06:24 +0200

    sleep

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+
+int main(int argc,char  *argv[]){
+  if(argc < 2){
+  fprintf(2, "Error");
+  exit(-1);
+
+
+  }
+  sleep(atoi(argv[1]));
+  exit(0);
+}
+
+
